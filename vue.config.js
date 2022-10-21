35fb2fe7cf4fdc543502925b0b3cca269ab5eabd
Xalunee <aidar2102003@gmail.com>
2022-10-21 10:44:58 +0500

    change vue.config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: '/web-note/',git 
+  publicPath: '/web-note/',
   transpileDependencies: true
 })
